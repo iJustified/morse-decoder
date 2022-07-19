edae73994e5eb97cc1669fe02a74863423529932
Ilia Tsygankov <ijustified@yandex.ru>
2022-07-19 18:46:03 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -39,6 +39,58 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    const arrExpr = []
+    let curStr = '';
+    
+    for(let i = 0; i < expr.length; i++) {
+      curStr += expr[i];
+      if (curStr.length == 10) {
+        arrExpr.push(curStr);
+        arrExpr.push('**');
+        curStr = '';
+      }
+    };
+    
+    const arrExprFlat = arrExpr.join('');
+    const newArrExpr = []
+    
+    for(let i = 0; i < arrExprFlat.length; i++) {
+      curStr += arrExprFlat[i];
+      if (curStr.length == 2) {
+        newArrExpr.push(curStr);
+        curStr = '';
+      }
+    };
+    
+    for(let i = 0; i < newArrExpr.length; i++) {
+      if(newArrExpr[i] == 10) {
+        newArrExpr[i] = '.';
+      } else if (newArrExpr[i] == 11) {
+        newArrExpr[i] = '-';
+      } else if (newArrExpr[i] == 00) {
+        newArrExpr[i] = '';
+      }
+    };
+    
+    const arrMorse = [newArrExpr.join('')];
+    const finalArrMorse = arrMorse.toString().split('**'); 
+    const result = [];
+    
+    for(let i = 0; i < finalArrMorse.length; i++) {
+      let currentKey = finalArrMorse[i];
+      let currentValue = Object.keys(MORSE_TABLE).indexOf(finalArrMorse[i])
+    
+      if (Object.keys(MORSE_TABLE).includes(currentKey)) {
+        result.push(Object.values(MORSE_TABLE)[currentValue]) 
+      } else {
+        result.push(' ') 
+      }
+    };
+    
+    const finalResult = result.join('');
+    const realyFinal = finalResult.split(' ').filter(e=>e).join(' ')
+
+    return realyFinal
 }
 
 module.exports = {
